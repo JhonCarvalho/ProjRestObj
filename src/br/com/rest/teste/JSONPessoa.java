75b235c72cb88ca64f71a53753b17e4a7682e705
unknown <jhoncarvalhosi@hotmail.com>
2019-04-05 21:23:28 -0300

    gt

---
+++ src/br/com/rest/teste/JSONPessoa.java
@@ -0,0 +1,33 @@
+package br.com.rest.teste;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+
+@Path("/json")
+public class JSONPessoa {
+
+	@GET
+	@Path("/get")
+	@Produces("application/json")
+	public Pessoa getProductInJSON() {
+		Pessoa p  = new Pessoa();
+		p.setCodigo(1);
+		p.setNome("José Silva");
+		
+		return p;
+	}
+	
+	@POST
+	@Path("/post")
+	@Consumes("application/json")
+	public Response createPessoaInJson(Pessoa pessoa) {
+		String result = "Product created : "+ pessoa;
+		return Response.status(201).entity(result).build();
+	}
+	
+}
